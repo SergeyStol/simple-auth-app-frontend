c1e86579bd47053c91cc0b6a599764f028de6566
sstol <sergeys@airoboticsdrones.com>
2023-10-11 15:24:50 +0300

    Change configuration for starting nginx server even server side is unavailable

--- build-and-deploy-in-nginx-docker.sh
+++ build-and-deploy-in-nginx-docker.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+readonly IMAGE_NAME="nginx:latest"
+readonly CONTAINER_NAME="nginx"
+readonly NETWORK_NAME="simple-auth-app"
+
+check_and_create_network() {
+    network_exists=$(docker network ls | grep "$NETWORK_NAME")
+
+    if [ -z "$network_exists" ]; then
+        docker network create "$NETWORK_NAME"
+        echo "Docker network $NETWORK_NAME created."
+    else
+        echo "Docker network $NETWORK_NAME already exists."
+    fi
+}
+
+check_and_remove_container() {
+  local container_name="$1"
+
+    container_exists=$(docker ps -a --filter "name=^${container_name}$" --format '{{.Names}}')
+
+    if [ -n "$container_exists" ]; then
+        docker rm -f "${container_name}"
+        echo "Docker container '${container_name}' removed."
+    else
+        echo "Docker container '${container_name}' does not exist."
+    fi
+}
+
+check_and_create_network
 rm -r dist
 parcel build index.html
-docker rm -f nginx
-docker run --name nginx --rm -v /$(pwd)/dist:/data/www:ro -v /$(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro -d -p 80:80 --network=simple-auth-app nginx
+check_and_remove_container "$CONTAINER_NAME"
+docker run --name "$CONTAINER_NAME" -v /$(pwd)/dist:/data/www:ro -v /$(pwd)/default.conf:/etc/nginx/conf.d/default.conf:ro -d -p 80:80 --network="$NETWORK_NAME" "$IMAGE_NAME"
